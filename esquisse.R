92922ae4e8ad22086dda90f427d30f0420fb0fed
danielw2904 <daniel.winkler@s.wu.ac.at>
2022-03-07 14:33:52 +0100

    upd

--- esquisse.R
+++ esquisse.R
@@ -2,12 +2,13 @@ library(colorspace)
 library(COVID19)
 library(esquisse)
 library(palmerpenguins)
+# comment
+# load covid data for austrian states
+covid_at <- covid19("AT", level = 2) # comment
+data("penguins") 
 
-covid_at <- covid19("AT", level = 2)
-data("penguins")
-
-esquisser(data = penguins)
-
+esquisser(data = penguins, viewer = "browser")
+?esquisser
 esquisser(data = covid_at)
 
 hcl_wizard()
