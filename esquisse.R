06806e7f215809dcd5751dd66353c97c23d0a71e
danielw2904 <daniel.winkler@s.wu.ac.at>
2022-03-09 16:33:51 +0100

    R esquisse update

--- esquisse.R
+++ esquisse.R
@@ -5,10 +5,20 @@ library(palmerpenguins)
 # comment
 # load covid data for austrian states
 covid_at <- covid19("AT", level = 2) # comment
+sum(covid_at$tests, na.rm = TRUE)
 data("penguins") 
 
 esquisser(data = penguins, viewer = "browser")
 ?esquisser
-esquisser(data = covid_at)
-
+esquisser(data = covid_at, viewer = "browser")
+plot(covid_at$date, covid_at$tests)
 hcl_wizard()
+library(readr)
+charts_global_at <- read_csv("data/charts_global_at.csv")
+View(charts_global_at)
+library(readr)
+movies <- read_csv(
+  "https://raw.githubusercontent.com/fivethirtyeight/data/master/bechdel/movies.csv")
+movies$domgross <- as.integer(movies$domgross)
+movies$intgross <- as.integer(movies$intgross)
+esquisser(movies, viewer = "browser")
